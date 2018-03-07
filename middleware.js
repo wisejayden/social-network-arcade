c688ab0e8277ab2ef496d0c2858979ca16c57973
Jayden Wise <jayden.wise@hotmail.com>
2018-03-07 12:18:39 +0100

    Removed unneccessary middleware functions

--- middleware.js
+++ middleware.js
@@ -1,45 +1,9 @@
-// Created middleware module.
-
-//If user tries to go to page when they do not have a cookie ,redirect them to the home page.
-
 var bcrypt = require('bcryptjs');
 const knox = require('knox');
 const fs = require('fs');
 let secrets;
 
 
-module.exports.checkForUser = function (req, res, next) {
-    if (req.session.user) {
-        console.log("User already logged in");
-        res.redirect('/profile');
-    } else {
-        next();
-    }
-};
-
-module.exports.checkCookie = function (req, res, next) {
-    if (!req.session.user) {
-        res.redirect('/register');
-    } else {
-        next();
-    }
-};
-
-module.exports.checkProfile = function(req, res, next) {
-    if(!req.session.user.profile) {
-        res.redirect('/profile');
-    } else {
-        next();
-    }
-};
-module.exports.checkForSignature = function(req, res, next) {
-    if(!req.session.user.hasSigned == true) {
-        console.log("You must sign before you can access these pages");
-        res.redirect('/petition');
-    } else {
-        next();
-    }
-};
 
 
 module.exports.hashPassword = function (plainTextPassword) {
@@ -99,15 +63,11 @@ module.exports.uploadToS3 = function(req, res, next) {
     });
     const readStream = fs.createReadStream(req.file.path);
     readStream.pipe(s3Request);
-
     //Next(), on successful response
     s3Request.on('response', s3Response => {
         const wasSuccessful = s3Response.statusCode == 200;
         if (wasSuccessful) {
-            console.log("Here is the link - https://s3.amazonaws.com/spicedling/jayden/" + req.file.filename);
             next();
-        } else {
-            console.log("Middleware Error");
         }
     });
 };
